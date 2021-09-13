91d526f1509527052156e55306b1e39d01df24c3
ancashoria <horia.ancas@gmail.com>
2021-09-13 12:34:40 +0300

    Added <Box />

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -1,5 +1,101 @@
-export function Box() {
-  return (
-    <div>box</div>
-  )
+import { createElement, AllHTMLAttributes, ElementType } from 'react'
+import cn from 'classnames'
+// import * as resetStyles from '../styles/reset.css'
+import { atoms, Atoms } from '../../../system/styles/sprinkles.css'
+
+export interface BoxProps extends Omit<
+  AllHTMLAttributes<HTMLElement>,
+  'content' | 'height' | 'translate' | 'color' | 'width' | 'cursor' | 'size'
+>,
+  Atoms {
+  component?: ElementType
+}
+
+export const Box = ({
+  component = 'div',
+  className,
+  padding,
+  paddingX,
+  paddingY,
+  paddingTop,
+  paddingBottom,
+  paddingLeft,
+  paddingRight,
+  margin,
+  marginX,
+  marginY,
+  marginTop,
+  marginBottom,
+  marginLeft,
+  marginRight,
+  display,
+  alignItems,
+  justifyContent,
+  flexDirection,
+  flexWrap,
+  flexGrow,
+  flexShrink,
+  borderRadius,
+  position,
+  top,
+  bottom,
+  left,
+  right,
+  background,
+  color,
+  width,
+  zIndex,
+  opacity,
+  pointerEvents,
+  cursor,
+  textAlign,
+  textTransform,
+  fontSize,
+  ...restProps
+}: BoxProps) => {
+  // TODO: get reset to work https://github.com/seek-oss/vanilla-extract/discussions/301
+  const atomClasses = atoms({
+    padding,
+    paddingX,
+    paddingY,
+    paddingTop,
+    paddingBottom,
+    paddingLeft,
+    paddingRight,
+    margin,
+    marginX,
+    marginY,
+    marginTop,
+    marginBottom,
+    marginLeft,
+    marginRight,
+    display,
+    alignItems,
+    justifyContent,
+    flexDirection,
+    flexWrap,
+    flexGrow,
+    flexShrink,
+    borderRadius,
+    position,
+    top,
+    bottom,
+    left,
+    right,
+    background,
+    color,
+    width,
+    zIndex,
+    opacity,
+    pointerEvents,
+    cursor,
+    textAlign,
+    fontSize,
+    textTransform,
+  })
+
+  return createElement(component, {
+    className: cn(atomClasses, className),
+    ...restProps
+  })
 }
