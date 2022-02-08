88e148ab251858f7ff554b967b418029e4c2ac8f
ancashoria <horia.ancas@gmail.com>
2022-02-08 18:30:17 +0200

    loading Google Tag Manager onLoad

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -81,6 +81,7 @@ export function Page({
 
       <Script
         id=""
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
